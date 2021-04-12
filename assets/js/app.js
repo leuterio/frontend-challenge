c03b3bfcfea55f55268920e415f30e620a81e64f
leuterio <jeffersonmoraes@live.com>
2021-04-12 15:11:48 -0300

    using fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,28 @@
 var loginButton = document.getElementById("login-button");
 
-loginButton.addEventListener("click",function(e){
-    console.log('clock');
+//document.querySelectorAll('.entrar')[0].style.display = 'none';
+//document.querySelectorAll('.smooth-spinner')[0].style.display = 'block';
+
+function login(email, password) {
+    fetch('https://reqres.in/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "email": email, "password": password }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            window.location.href = "/pages/lista-usuarios.html";
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+loginButton.addEventListener("click", function (e) {
     var email = document.getElementById("email").value;
     var senha = document.getElementById("senha").value;
     login(email, senha);
-},false);
-
-function login(email, password) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://reqres.in/api/login", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        email: email,
-        password: password
-    }));
-    xhr.onload = function () {
-        document.querySelectorAll('.entrar')[0].style.display = 'none';
-        document.querySelectorAll('.smooth-spinner')[0].style.display = 'block';
-
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            window.location.href = "/pages/lista-usuarios.html";
-        }
-
-        console.log(this.responseText);
-        var data = JSON.parse(this.responseText);
-        console.log(data);
-    }
-}
+}, false);
